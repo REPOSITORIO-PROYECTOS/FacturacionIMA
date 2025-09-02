17b66bbd3b4b3bf59fadb05e15e9b65de37811d0
Lucas Mazzoni <lucas.mazzoni@hotmail.com>
2025-09-02 15:40:39 -0300

    new endpoints boletas

--- backend/app/blueprints/boletas.py
+++ backend/app/blueprints/boletas.py
@@ -16,3 +16,28 @@ def traer_boletas():
         return boletas
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Ocurrió un error inesperado: {e}")
+
+
+@router.get("/obtener-no-facturadas", response_model=List[Dict[str, Any]])
+def traer_boletas():
+    handler = TablasHandler()
+    try:
+        boletas = handler.cargar_ingresos()
+        boletas_filtradas = [b for b in boletas if b.get("facturacion") == "falta facturar"]
+        return boletas_filtradas
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Ocurrió un error inesperado: {e}")
+    
+
+
+@router.get("/obtener-facturadas", response_model=List[Dict[str, Any]])
+def traer_boletas():
+    handler = TablasHandler()
+    try:
+        boletas = handler.cargar_ingresos()
+        boletas_filtradas = [b for b in boletas if b.get("facturacion") == "facturado"]
+        return boletas_filtradas
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Ocurrió un error inesperado: {e}")
+    
+
